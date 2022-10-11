24168cddf788820ab9f01926e986df39a5ebb2a8
Magnus Hodne <magnus@drobak.org>
2022-10-11 08:47:37 +0200

    Changed to get-mapping

--- devops-test/src/main/kotlin/no/kristiania/devopstest/controller/HelloController.kt
+++ devops-test/src/main/kotlin/no/kristiania/devopstest/controller/HelloController.kt
@@ -1,5 +1,6 @@
 package no.kristiania.devopstest.controller
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/hello")
 class HelloController {
 
-        @RequestMapping
+        @GetMapping
         fun hello(): String {
             return "Hello World!"
         }
